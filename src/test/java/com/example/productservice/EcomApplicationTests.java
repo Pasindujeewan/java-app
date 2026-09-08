a8e9d4e301cc4280f6d8bb84b185ade240f2bdb2
unknown <213911056+Pasindujeewan@users.noreply.github.com>
2026-09-08 23:42:13 +0530

    feat:add test containers dependencies

--- src/test/java/com/example/productservice/EcomApplicationTests.java
+++ src/test/java/com/example/productservice/EcomApplicationTests.java
@@ -2,10 +2,22 @@ package com.example.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @SpringBootTest
+@Testcontainers
 class EcomApplicationTests {
 
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    static void setProperties (DynamicPropertyRegistry dynamicPropertyRegistry){
+            dynamicPropertyRegistry.add("spring.data.mongodb.uri",mongoDBContainer::getReplicaSetUrl);
+    }
     @Test
     void contextLoads() {
     }
